9439e5b31c13ff96dd045075bf6ce76b17119e43
KeTav <ketavgaming@gmail.com>
2020-05-21 10:11:33 +0700

    Init project

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # web-practice
 
 A repository for web practice.
+
+### Used:
+
+1. [bootstrap-4.5.0](https://github.com/twbs/bootstrap)
+1. [DevTips-Starter-Kit](https://github.com/DevTips/DevTips-Starter-Kit)
---
+++ assets/js/lab6.js
@@ -0,0 +1,35 @@
+"use strict";
+
+function seqSum(seq) {
+  let sum = 0;
+  for (let i = 0; i < seq.length; ++i) {
+    sum += seq[i];
+  }
+  return sum;
+}
+
+function minSeqElem(seq) {
+  let minElem = seq[0];
+  for (let i = 1; i < seq.length; ++i) {
+    minElem = (seq[i] < minElem) ? seq[i] : minElem;
+  }
+  return minElem;
+}
+
+let seq = prompt("Enter a sequence of elements:", "3 1 2").split(' ')
+seq = seq.map(function (item) {
+  return parseInt(item);
+});
+
+let secondMaxSeqElem = seq => seq.sort( (a, b) => a - b )[seq.length - 2];
+
+let sortedSeq = seq.slice();
+sortedSeq.sort( (a, b) => a - b );
+let result = `
+  Entered sequence of elements: ${seq}
+  Sorted sequence of elements: ${sortedSeq}
+  Sequence sum: ${seqSum(seq)}
+  Minimum sequence element: ${minSeqElem(seq)}
+  The second largest element in the sequence: ${secondMaxSeqElem(seq)}
+`;
+alert(result);
